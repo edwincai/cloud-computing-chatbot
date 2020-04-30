63c010b205456d9bf82525ce355e04fbe464d569
ra7more <ra7more@outlook.com>
2020-04-30 14:37:55 +0800

    init chatbox

--- chatdemo.py
+++ chatdemo.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
 import asyncio
 import tornado.escape
 import tornado.ioloop
@@ -55,7 +39,6 @@ class MessageBuffer(object):
         self.cond.notify_all()
 
 
-# Making this a non-singleton is left as an exercise for the reader.
 global_message_buffer = MessageBuffer()
 
 
